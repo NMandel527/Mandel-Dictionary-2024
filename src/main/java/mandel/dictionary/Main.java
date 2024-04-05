518de127ffa5215a52fe5014141fc275f33fb7a7
NMandel527 <nmandel527@gmail.com>
2024-04-05 18:07:47 -0400

    dictionary gui with checkstyles

--- src/main/java/mandel/dictionary/Main.java
+++ src/main/java/mandel/dictionary/Main.java
@@ -6,7 +6,7 @@ public class Main {
         try {
             edf = new EnglishDictionaryFrame();
             edf.setVisible(true);
-        } catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
